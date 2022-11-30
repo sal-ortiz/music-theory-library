449ebdb0a85c0c876d57586a3593127b29331cb0
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2022-11-30 12:43:22 -0600

    minor cleanup to base lib test fixtures

--- __tests__/__support__/base_helpers.js
+++ __tests__/__support__/base_helpers.js
@@ -39,13 +39,15 @@ global.INTERVAL_PARSE_EXPECTATIONS = [
   { input: '-3', output: { quality: '-', steps: 3 } },
   { input: 'm3', output: { quality: 'm', steps: 3 } },
   { input: '+2', output: { quality: '+', steps: 2 } },
+
   { input: 'M3', output: { quality: 'M', steps: 3 } },
   { input: '+3', output: { quality: '+', steps: 3 } },
   { input: '-4', output: { quality: '-', steps: 4 } },
   { input: 'P4', output: { quality: 'P', steps: 4 } },
   { input: '+4', output: { quality: '+', steps: 4 } },
   { input: '-5', output: { quality: '-', steps: 5 } },
-  { input: 'M5', output: { quality: 'M', steps: 5 } },
+  { input: 'P5', output: { quality: 'P', steps: 5 } },
+
   { input: '+5', output: { quality: '+', steps: 5 } },
   { input: '-6', output: { quality: '-', steps: 6 } },
   { input: 'm6', output: { quality: 'm', steps: 6 } },
@@ -81,12 +83,12 @@ global.TRANSPOSE_UP_EXPECTATIONS = [
   { input: { note: 'A5',  semitones: 7 }, output: 'E6'  },
   { input: { note: 'A#5', semitones: 9 }, output: 'G6' },
 
-  { input: { note: 'Bb6', semitones: 6 }, output: 'E7' },
-  { input: { note: 'B6',  semitones: 8 }, output: 'G7'  },
+  { input: { note: 'Bb6', semitones: 6 },  output: 'E7' },
+  { input: { note: 'B6',  semitones: 8 },  output: 'G7'  },
   { input: { note: 'B#6', semitones: 10 }, output: 'A#6' },
 
-  { input: { note: 'Cb7', semitones: 7 }, output: 'F#8' },
-  { input: { note: 'C7',  semitones: 9 }, output: 'A7'  },
+  { input: { note: 'Cb7', semitones: 7 },  output: 'F#8' },
+  { input: { note: 'C7',  semitones: 9 },  output: 'A7'  },
   { input: { note: 'C#7', semitones: 11 }, output: 'C8' },
 
   { input: { note: 'Db8', semitones: 8 },  output: 'A8' },
@@ -115,46 +117,41 @@ global.TRANSPOSE_DOWN_EXPECTATIONS = [
   { input: { note: 'F5',  semitones: -7 }, output: 'A#4'  },
   { input: { note: 'F#5', semitones: -9 }, output: 'A4' },
 
-  { input: { note: 'Eb4', semitones: -6 }, output: 'A3' },
-  { input: { note: 'E4',  semitones: -8 }, output: 'G#3'  },
+  { input: { note: 'Eb4', semitones: -6 },  output: 'A3' },
+  { input: { note: 'E4',  semitones: -8 },  output: 'G#3'  },
   { input: { note: 'E#4', semitones: -10 }, output: 'G3' },
 
-  { input: { note: 'Db3', semitones: -7 }, output: 'F#2' },
-  { input: { note: 'D3',  semitones: -9 }, output: 'F2'  },
+  { input: { note: 'Db3', semitones: -7 },  output: 'F#2' },
+  { input: { note: 'D3',  semitones: -9 },  output: 'F2'  },
   { input: { note: 'D#3', semitones: -11 }, output: 'E2' },
 
-  { input: { note: 'Cb2', semitones: -8 }, output: 'D#2' },
+  { input: { note: 'Cb2', semitones: -8 },  output: 'D#2' },
   { input: { note: 'C2',  semitones: -10 }, output: 'D1'  },
   { input: { note: 'C#2', semitones: -12 }, output: 'C#1' },
 
-  { input: { note: 'Bb1', semitones: -9 }, output: 'C#1' },
+  { input: { note: 'Bb1', semitones: -9 },  output: 'C#1' },
   { input: { note: 'B1',  semitones: -11 }, output: 'C1'  },
   { input: { note: 'C2',  semitones: -13 }, output: 'B0'  },
 ];
 
 global.TREBLE_NOTE_EXPECTATIONS = [
-  { input: { note: 'C0',  semitones: 0 }, output: 'C0' },
-  { input: { note: 'C#0', semitones: 1 }, output: 'D0' },
+  { input: { note: 'C0',  semitones: 0 },  output: 'C0' },
+  { input: { note: 'C#0', semitones: 1 },  output: 'D0' },
   { input: { note: 'D0',  semitones: 2 },  output: 'E0' },
-  { input: { note: 'D#0',  semitones: 3 },  output: 'F#0' },
+  { input: { note: 'D#0',  semitones: 3 }, output: 'F#0' },
 
   { input: { note: 'E0',  semitones: 4 }, output: 'G#0' },
-  { input: { note: 'F0', semitones: 5 }, output: 'A#0' },
-  { input: { note: 'G0',  semitones: 6 },  output: 'C#1' },
-  { input: { note: 'G#0',  semitones: 7 },  output: 'D#1' },
+  { input: { note: 'F0', semitones: 5 },  output: 'A#0' },
+  { input: { note: 'F#0', semitones: 6 }, output: 'C1' },
+  { input: { note: 'G0',  semitones: 7 }, output: 'D1' },
 
-  { input: { note: 'A0',  semitones: 8 }, output: 'F1' },
-  { input: { note: 'A#0', semitones: 9 }, output: 'G1' },
-  { input: { note: 'B0',  semitones: 10 },  output: 'A1' },
-  { input: { note: 'C1',  semitones: 11 },  output: 'B1' },
+  { input: { note: 'G#0',  semitones: 8 }, output: 'E1' },
+  { input: { note: 'A0',  semitones: 9 },  output: 'F#1' },
+  { input: { note: 'A#0', semitones: 10 }, output: 'G#1' },
+  { input: { note: 'B0',  semitones: 11 }, output: 'A#1' },
 
-  { input: { note: 'C#1',  semitones: 8 }, output: 'A1' },
-  { input: { note: 'D1', semitones: 9 }, output: 'B1' },
-  { input: { note: 'D#1',  semitones: 10 },  output: 'C#2' },
-  { input: { note: 'E1',  semitones: 11 },  output: 'D#2' },
-
-  { input: { note: 'F1',  semitones: 12 }, output: 'F2' },
-  { input: { note: 'F#1', semitones: 13 }, output: 'G2' },
-  { input: { note: 'G1',  semitones: 14 },  output: 'A2' },
-  { input: { note: 'G#1',  semitones: 15 },  output: 'B2' },
+  { input: { note: 'C1',  semitones: 12 },  output: 'C2' },
+  { input: { note: 'C#1',  semitones: 13 }, output: 'D2' },
+  { input: { note: 'D1', semitones: 14 },   output: 'E2' },
+  { input: { note: 'D#1',  semitones: 15 }, output: 'F#2' },
 ];
