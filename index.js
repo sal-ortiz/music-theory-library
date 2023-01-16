a429dd6762fc055d641022e077501543711c3e3c
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2023-01-16 17:09:05 -0600

    fixed index for importing

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 const Path = require('path');
 
 const libPath = Path.join(__dirname, 'lib');
-const Note = require(Path.join(libPath, 'note.js'));
-const Interval = require(Path.join(libPath, 'interval.js'));
-const Helpers = require(Path.join(libPath, 'helpers.js'));
+//const Note = require(Path.join(libPath, 'note.js'));
+//const Interval = require(Path.join(libPath, 'interval.js'));
+//const Helpers = require(Path.join(libPath, 'helpers.js'));
 
-
+module.exports = {
+  Chord: require(Path.join(libPath, 'chord.js')),
+  Note: require(Path.join(libPath, 'note.js')),
+  Interval: require(Path.join(libPath, 'interval.js')),
+  //Helpers: require(Path.join(libPath, 'helpers.js')),
+};
